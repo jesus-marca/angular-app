eaf7f6fcf2f6cff13f4ee5cfe28fe850a6e14148
braulio-marca <braulio.marca@unmsm.edu.pe>
2024-08-28 19:29:51 -0500

    second commit

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rgister-page',
   templateUrl: './register-page.component.html',
-  styleUrl: './Register-page.component.css'
+  styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
 
